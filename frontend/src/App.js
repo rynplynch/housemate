330bf57a859035bc4d09d262a30b791b70f04104
rynplynch <rynplynch@gmail.com>
2023-03-09 20:46:42 -0800

    styling changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import React, {} from 'react'
-import Dash from './pages/Dashboard'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import TestPage from './pages/TestPage'
@@ -14,7 +13,6 @@ function App() {
       <Routes>
         <Route  exact path='/login' element={<LoginPage />} />
         <Route  exact path='/register' element={<RegisterPage />} />
-        <Route exact path='/dashboard' element={<Dash />} />
         <Route exact path='/test' element={<TestPage />} />
         <Route exact path='/house' element={<HousePage />} />
         <Route path="*" element={<Navigate to="login" replace />} />
